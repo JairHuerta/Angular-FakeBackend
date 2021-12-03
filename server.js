a545a933bc9204396a02695474d1926e1b61d4fa
JairHuerta <jair.17huerta@gmail.com>
2021-12-03 12:52:07 -0600

    intento 5 subir a heroku

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+// app.use(express.static('./dist/angular-registration-login-example'));
+
+// app.get('/*', (req, res) => {
+//     res.sendFile('index.html', {root: 'dist/angular-registration-login-example/'})
+// });
+
+// app.listen(process.env.PORT || 8080);
+
+const path = require('path');
 const express = require('express');
-const path = require('path')
+const app = express();
 
-const app = express()
+// Serve static files
+app.use(express.static(__dirname + '/dist/angular-registration-login-example'));
 
-app.use(express.static('./dist/angular-registration-login-example'));
-
-app.get('/*', (req, res) => {
-    res.sendFile('index.html', {root: 'dist/angular-registration-login-example/'})
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/angular-registration-login-example/index.html'));
 });
 
+// default Heroku port
 app.listen(process.env.PORT || 8080);
